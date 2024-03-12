226e642d96d9d7e5038acfc2cd367d552f9666a0
Agnes Martinsson <agnesdlmartinsson@gmail.com>
2024-03-12 23:18:26 +0100

    Make tmux statusbar theme dynamic

--- modules/home/features/tmux/default.nix
+++ modules/home/features/tmux/default.nix
@@ -5,6 +5,12 @@
     baseIndex = 1;
     escapeTime = 0;
     keyMode = "vi";
-    extraConfig = builtins.readFile ./tmux.conf;
+    extraConfig = with config.colorScheme.palette; ''      
+      # Theming
+      set -g status-style 'bg=#${base00} fg=#${base06}'
+      
+      # Draw the rest of the owl
+      ${builtins.readFile ./tmux.conf}
+    '';
   };
 }
