8567ce15c662bafb632d1247149bd4dce5fc6891
Agnes Martinsson <agnesdlmartinsson@gmail.com>
2024-03-18 13:10:50 +0100

    Add binds for screen dimming

--- modules/home/features/i3/default.nix
+++ modules/home/features/i3/default.nix
@@ -139,8 +139,8 @@ in {
         "XF86AudioMute" = "exec --no-startup-id pactl set-sink-mute 0 toggle"; # mute sound
         #
         # # Sreen brightness controls
-        "XF86MonBrightnessUp" = "exec xbacklight -inc 20"; # increase screen brightness
-        "XF86MonBrightnessDown" = "exec xbacklight -dec 20"; # decrease screen brightness
+        "XF86MonBrightnessUp" = "exec brightnessctl set 20%+"; # increase screen brightness
+        "XF86MonBrightnessDown" = "exec brightnessctl set 20%-"; # decrease screen brightness
       };
 
       bars = [
